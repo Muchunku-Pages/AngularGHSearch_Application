e91c9c8c30d205fe896ef82c2474f13d28aab89b
Brian Muchunku <brauxer@gmail.com>
2022-01-17 21:08:27 +0300

    Set up Routing between our Search application's 3 component template views by adding thier Classes into the routing module and specifying the route paths between their respective template views

--- AngularRouting/NgHubSearchApplication/src/app/app-routing.module.ts
+++ AngularRouting/NgHubSearchApplication/src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {SearchComponent} from './search/search.component';
+import {UserProfileComponent} from './user-profile/user-profile.component';
+import {RepositoryDetailsComponent} from './repository-details/repository-details.component';
 
 const routes: Routes = [
 { path:"", component:SearchComponent },
 
-];
+{path:"user-profile/:githubUsername", component:UserProfileComponent},
+
+{path:"repository-details/:githubUsername", component:RepositoryDetailsComponent}
+
+   ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
