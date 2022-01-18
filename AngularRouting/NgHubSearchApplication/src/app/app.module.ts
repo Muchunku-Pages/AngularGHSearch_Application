1502d6213ebdc12bff3c8ac7cf0bbfa753fb58c5
Brian Muchunku <brauxer@gmail.com>
2022-01-18 16:51:50 +0300

    Install the FortAwesome library from the official repository and add the FontAwesomeModule to our NgHubSearch application's main module to make it available for use by the relevant component template views

--- AngularRouting/NgHubSearchApplication/src/app/app.module.ts
+++ AngularRouting/NgHubSearchApplication/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {FormsModule,ReactiveFormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {NgbModule, NgbNavModule} from '@ng-bootstrap/ng-bootstrap';
 import { SearchComponent } from './search/search.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { RepositoryDetailsComponent } from './repository-details/repository-details.component';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,9 @@ import { RepositoryDetailsComponent } from './repository-details/repository-deta
     FormsModule, 
     ReactiveFormsModule,
     NgbModule,
-    HttpClientModule
+    NgbNavModule,
+    HttpClientModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
